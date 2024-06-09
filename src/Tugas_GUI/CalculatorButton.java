519d803923cd035882af3fb728e0fa29fa2345ad
Aldi setiawan <aldi60051@gmail.com>
2024-06-09 17:02:43 +0700

    Tugas GUI

---
+++ src/Tugas_GUI/CalculatorButton.java
@@ -0,0 +1,9 @@
+package Tugas_GUI;
+
+import javax.swing.*;
+
+public class CalculatorButton extends JButton {
+    public CalculatorButton(String text) {
+        super(text);
+    }
+}
---
+++ src/Tugas_GUI/CalculatorFrame.java
@@ -0,0 +1,21 @@
+package Tugas_GUI;
+
+import javax.swing.*;
+public class CalculatorFrame extends JFrame {
+    public CalculatorFrame() {
+        setTitle("Kalkulator");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 300);
+        setLocationRelativeTo(null);
+
+        CalculatorPanel panel = new CalculatorPanel();
+        add(panel);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            CalculatorFrame frame = new CalculatorFrame();
+            frame.setVisible(true);
+        });
+    }
+}
